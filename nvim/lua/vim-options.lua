51f7437fb6c99d2135c6a7788f523b5ebd8ce964
Nabil Aldihni <aldihninabil@gmail.com>
2025-01-14 17:59:34 -0500

    Create vim-options.lua

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.nu = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.hidden = true
+
+vim.keymap.set("n", "<Enter>", "o<Esc>")
+vim.keymap.set("n", "<S-Enter>", "O<Esc>")
