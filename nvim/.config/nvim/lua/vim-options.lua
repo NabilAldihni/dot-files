2e8545600940a8d40afe97a0004777123b9bcf19
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2025-06-25 16:58:01 -0400

    Initial files

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,36 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.nu = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.hidden = true
+
+vim.keymap.set("n", "<Enter>", "o<Esc>")
+vim.keymap.set("n", "<S-Enter>", "O<Esc>")
+vim.keymap.set('n', '<C-c>', '<cmd>nohlsearch<CR>')
+
+-- Disable inline text
+vim.diagnostic.config({
+  virtual_text = false,
+  signs        = true,   -- keep gutter signs
+  underline    = true,
+  update_in_insert = false,
+})
+
+-- Always show the signcolumn
+vim.o.signcolumn = "yes"
+
+-- Map <Leader>e (or any key) to show diagnostics in a floating window
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { silent = true })
+
