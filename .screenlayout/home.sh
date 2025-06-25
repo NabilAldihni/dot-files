1ffc95374e7d49caaf21d0002f58e5821a115fe7
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2025-06-25 16:47:58 -0400

    Reset the repo

--- .screenlayout/home.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --primary --mode 1280x800 --pos 0x402 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output DP-3-5 --mode 1920x1080 --pos 3200x536 --rotate normal --output DP-3-6 --mode 1920x1080 --pos 1280x0 --rotate normal
--- README.md
+++
@@ -1,6 +0,0 @@
-Just run the following to setup the dotfiles
-
-
-```shell
-curl https://raw.githubusercontent.com/NabilAldihni/dot-files/master/scripts/setup | bash
-```
