32675ee77014208a52ebf7a8b3719dc6f7abac6f
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2025-07-03 01:03:40 -0400

    added markdown

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -6,6 +6,16 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
+
+      -- Custom browser open function (Lua version of Vimscript function)
+      vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+
+      -- Define Vimscript function in Lua-compatible way
+      vim.cmd([[
+        function! OpenMarkdownPreview(url)
+          execute "silent !firefox --new-window " . a:url
+        endfunction
+      ]])
     end,
   },
 }
