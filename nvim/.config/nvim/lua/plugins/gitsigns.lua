948448314f5015198a42fc8d4ad79971a7475514
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2026-03-03 23:21:09 -0500

    added git blames

---
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,23 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup({
+      signs = {
+        add          = { text = '│' },
+        change       = { text = '│' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol',
+        delay = 500,
+        ignore_whitespace = false,
+      },
+      current_line_blame_formatter = '        <author>, <author_time:%Y-%m-%d> - <summary>',
+    })
+  end,
+}
