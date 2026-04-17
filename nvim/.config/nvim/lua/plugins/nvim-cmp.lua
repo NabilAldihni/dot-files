42f5cba9f63a457b60fe04e05bced13b12362d85
Nabil Aldihni Garcia <aldihninabil@gmail.com>
2026-04-16 22:05:21 -0400

    fixed nvim autocomplete and waybar wallpaper

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -46,7 +46,13 @@ return {
                 ["<C-d>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<CR>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() and cmp.get_active_entry() then
+                        cmp.confirm({ select = false })
+                    else
+                        fallback()
+                    end
+                end),
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
